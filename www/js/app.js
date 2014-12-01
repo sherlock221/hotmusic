588098407b235b213969be81624b997b51aec29b
sherlock <920825209@qq.com>
2014-12-02 01:01:59 +0800

    注册功能完成

--- www/js/app.js
+++ www/js/app.js
@@ -2,6 +2,8 @@ angular.module('hotmusic', ['ionic'])
 
     .run(function ($ionicPlatform) {
         $ionicPlatform.ready(function () {
+        
+        	console.log("dddd");
             // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
             // for form inputs)
             if (window.cordova && window.cordova.plugins.Keyboard) {
